172c82c56c657622f76a814d3b858f9dd6beea48
Oleg Shakhmatov <oleg.shakhmatov@billetkontoret.dk>
2024-05-23 09:12:06 +0200

    add ru culture to date parsing

--- src/LifeMastery.Core/Modules/WeightControl/Commands/RemoveWeightRecord.cs
+++ src/LifeMastery.Core/Modules/WeightControl/Commands/RemoveWeightRecord.cs
@@ -1,5 +1,6 @@
 ﻿using LifeMastery.Core.Common;
 using LifeMastery.Core.Modules.WeightControl.Repositories;
+using System.Globalization;
 
 namespace LifeMastery.Core.Modules.WeightControl.Commands;
 
@@ -14,7 +15,7 @@ public sealed class RemoveWeightRecord : CommandBase<string>
 
     protected override async Task OnExecute(string date, CancellationToken token)
     {
-        var dateOnly = DateOnly.Parse(date);
+        var dateOnly = DateOnly.Parse(date, new CultureInfo("ru-RU"));
 
         var weightRecord = await weightRecordRepository.Get(dateOnly)
             ?? throw new Exception($"WeightRecord with Date={dateOnly} was not found");
